8b6847d54faf47d0f5c551328a107cbf0d529308
lousteez <louissterrett@gmail.com>
2021-05-19 17:38:01 -0500

    initial commit

---
+++ localtag/src/routes.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Main } from './components/Main'
+import { Cards } from './components/Cards'
+import { Route, Switch, Redirect } from 'react-router-dom'
+
+export const Routes = () => {
+    return (
+        <div>
+            <Switch>
+                <Route exact path="/cards" component={Cards} />
+            </Switch>
+        </div>
+    )
+}

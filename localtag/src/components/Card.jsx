68ca26782fd6eaf2eed887760356268c39bffb7a
latham <lathamstevenjohnson@gmail.com>
2021-05-19 15:45:21 -0400

    updated fetch with useEffect

--- localtag/src/components/Card.jsx
+++ localtag/src/components/Card.jsx
@@ -1,18 +1,20 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 function Card({card}) {
     const [image, setImage] = useState('[]')
     const [location, setLocation] = useState('[]')
-
-    fetch('http://localhost:4000/images/' + card.image)
-        .then(res => res.json())
-        .then(data => setImage(data))
-        .catch(console.error)
-
-    fetch('http://localhost:4000/locations/' + card.location)
-        .then(res => res.json())
-        .then(data => setLocation(data))
-        .catch(console.error)
+    
+    useEffect(() => {
+        fetch('http://localhost:4000/images/' + card.image)
+            .then(res => res.json())
+            .then(data => setImage(data))
+            .catch(console.error)
+        fetch('http://localhost:4000/locations/' + card.location)
+            .then(res => res.json())
+            .then(data => setLocation(data))
+            .catch(console.error)
+    },[])
+    
         
     return (
         <span>
--- localtag/src/components/Cards.jsx
+++ localtag/src/components/Cards.jsx
@@ -1,13 +1,17 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Card from './Card'
 
 function Cards(props) {
     const [cards, setCards] = useState([])
 
-    fetch('http://localhost:4000/cards')
-        .then(res => res.json())
-        .then(data => setCards(data))
-        .catch(console.error)
+    useEffect(() => {
+        fetch('http://localhost:4000/cards')
+            .then(res => res.json())
+            .then(data => setCards(data))
+            .catch(console.error)
+    }, [])
+    
+
     return (
         <div>
             {cards.map(card => {

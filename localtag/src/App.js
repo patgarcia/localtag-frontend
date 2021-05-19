9f2ca3f0409a99c225143678b205fb11ae3d2c87
lousteez <louissterrett@gmail.com>
2021-05-19 14:25:33 -0500

    Skeleton of Header components and its components; added src/tools/tools.js

--- localtag/src/App.js
+++ localtag/src/App.js
@@ -2,11 +2,13 @@ import React, { useState, useContext } from 'react'
 import { DataContext } from './components/DataContext' 
 import Main from './components/Main'
 import './App.css';
+import Header from './components/Header'
 
 function App() {
   return (
       <DataContext.Provider>
-        <Main/>
+        {/* <h1>LocalTag Frontend</h1> */}
+        <Header />
       </DataContext.Provider>
   );
 }
--- localtag/src/components/Header.jsx
+++ localtag/src/components/Header.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import SearchBar from './SearchBar'
-import Logo from './Logo'
-import NewButton from './NewButton'
-import EditForm from './EditForm'
-import CamButton from './CamButton'
+import React, { useEffect, useState } from "react";
+import SearchBar from "./SearchBar";
+import Logo from "./Logo";
+import NewButton from "./NewButton";
+import EditForm from "./EditForm";
+import CamButton from "./CamButton";
 
 function Header(props) {
-    return (
-        <div>
-            <h1>Local Tag</h1>
-            <SearchBar />
-            <Logo />
-            <NewButton />
-            <EditForm />
-            <CamButton />
-        </div>
-    );
+  return (
+    <div>
+      <h1>Local Tag</h1>
+      <SearchBar />
+      <Logo />
+      <NewButton />
+      <EditForm />
+      <CamButton />
+    </div>
+  );
 }
 
 export default Header;
